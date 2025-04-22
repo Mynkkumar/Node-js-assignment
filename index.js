a19380c9e4ba1e211c1568021340d425189b695d
Mayank Kumar <Mayank.Kumar7@gds.ey.com>
2025-04-23 01:09:48 +0530

    used mvc model to maintain the structure of file

--- index.js
+++ index.js
@@ -2,67 +2,20 @@
 const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user')
 
-const index = fs.readFileSync("./index.html", "utf-8");
+// const index = fs.readFileSync("./index.html", "utf-8");
 const data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
-const products = data.products;
+// const products = data.products;
 
 const server = express();
 
 server.use(morgan('default'));
-server.use(express.json())
+server.use(express.json());
 server.use(express.static('public'));
-
-// API - Endpoints
-
-// C R U D
-
-// Create POST /products
-server.post('/products',(req, res) => {
-    console.log(req.body);
-    products.push(req.body);
-    res.status(201).json(req.body);
-})
-
-// Read GET /products
-server.get('/products', (req, res) => {
-    res.json(products);
-})
-
-// Read GET /products/:id
-server.get('/products/:id', (req, res) => {
-    const id = +req.params.id;
-    const product = products.find(p=> p.id === id);
-    res.json(product);
-})
-
-// Update PUT /products/:id
-
-server.put('/products/:id', (req, res)=> {
-    const id = +req.params.id;
-    const productIndex = products.findIndex(p => p.id === id);
-    const product = products.splice(productIndex, 1, {...req.body, id:id});
-    res.status(201).json();
-})
-
-// Update PATCH /products/:id
-server.patch('/products/:id', (req, res) => {
-    const id = +req.params.id;
-    const productIndex = products.findIndex(p => p.id === id)
-    const product = products[productIndex];
-    products.splice(productIndex, 1, {...product, ...req.body});
-    res.status(201).json();
-})
-
-//Delete DELETE /products/:id
-server.delete('/products/:id', (req, res) => {
-    const id = +req.params.id;
-    const productIndex = products.findIndex(p => p.id === id)
-    const product = products[productIndex];
-    products.splice(productIndex, 1);
-    res.status(201).json(product)
-})
-
+server.use('/products', productRouter.router);
+server.use('/users', userRouter.router)
 
 server.listen(8080, () => {
     console.log("server started");
